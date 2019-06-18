1e7ef5a27670e01313d811e228b565710f7134f4
Bruno Arruda <arrudabruno10@gmail.com>
2019-06-17 21:25:03 -0300

    Verifica a conjugação de gênero ao fazer replace()
    Agora as frases podem conservar melhor o sentido após a substituição.
    Adicionei .gitignore para evitar o versionamento de arquivos do VSCode

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var elements = document.getElementsByTagName("*");
 
+// base de um regex para capturar um artigo, com contrações de preposições ou plural
+const regexForWordAndArticle = "((?:\bun|\buma?|(?:\bd|\bn|\bpr|\bpel|\b|)[oa]|à)s? )?({0})";
+
 window.onload = function() {
   checkElements();
   window.setInterval(() => {
@@ -15,6 +18,55 @@ document.addEventListener(
   false
 );
 
+function makeRegexWithArticle(word) {
+  return new RegExp(regexForWordAndArticle.replace("{0}", word), "gi");
+};
+
+function changeWordAndConjugate(match, p1, p2) {
+  if (p1 === undefined || p1 === null) {
+    // necessário para não concatenar 'undefined'
+    p1 = "";
+  }  else {
+    // operações de inversão de gênero
+    if (p1 == "umas") {
+      p1 = "uns";
+    } else if (p1 == "uns") {
+      p1 = "umas";
+    }
+    if (p1 == "uma") {
+      p1 = "um";
+    } else if (p1 === "um") {
+      p1 = "uma";
+    }
+    if (p1.search("a") != -1) {
+      p1 = p1.replace("a", "o");
+    } else {
+      p1 = p1.replace("o", "a");
+    }
+    if (p1.search("à") != -1) {
+      p1 = p1.replace("à", "ao")
+    }
+  }
+
+  // as substituições não podem ser passadas via argumento
+  if (p2 == "governo") {
+    p2 = "Organização Criminosa";
+  } else if (p2 == "tributação") {
+    p2 = "pagamento forçado";
+  } else if (p2 == "tributações") {
+    p2 = "pagamentos forçados";
+  } else if(p2 == "loteria") {
+    p2 = "esquema de Pirâmide Estatal";
+  } else if (p2 == "mega-sena") {
+    p2 = "esquema de Pirâmide Estatal"
+  } else if("constituição") {
+    p2 = "Guardanapo Sujo";
+  } else {
+    throw `Nada para substituir o termo ${match}`;
+  }
+  return `${p1}${p2}`
+};
+
 function checkElements() {
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -25,11 +77,11 @@ function checkElements() {
       if (node.nodeType === 3) {
         var text = node.nodeValue;
         var replacedText = text
-          .replace(/\bgoverno\b/gi, "Organização Criminosa")
+          .replace(makeRegexWithArticle("governo"), changeWordAndConjugate)
           .replace(/\bexcelentíssimo\b/gi, "indigníssimo")
           .replace(/\bBrasil\b/gi, "Bananíl")
-          .replace(/\btributação\b/gi, "pagamento forçado")
-          .replace(/\btributações\b/gi, "pagamentos forçados")
+          .replace(makeRegexWithArticle("tributação"), changeWordAndConjugate)
+          .replace(makeRegexWithArticle("tributações"), changeWordAndConjugate)
           .replace(/\bimposto\b/gi, "roubo")
           .replace(/\blegislação\b/gi, "regra da Máfia")
           .replace(/\blegislações\b/gi, "regras da Máfia")
@@ -48,8 +100,8 @@ function checkElements() {
           .replace(/\bpresidente\b/gi, "Chefe da Máfia")
           .replace(/\bpresidentes\b/gi, "Gangue Mafiosa")
           .replace(/\bprevidência\b/gi, "Pirâmide Estatal")
-          .replace(/\bloteria\b/gi, "esquema de Pirâmide Estatal")
-          .replace(/mega[\s\-]sena/gi, "esquema de Pirâmide Estatal")
+          .replace(makeRegexWithArticle("loteria"), changeWordAndConjugate)
+          .replace(makeRegexWithArticle("mega-sena"), changeWordAndConjugate)
           .replace(/\bmilitantes\b/gi, "militontos")
           .replace(/\bmilitante\b/gi, "militonto")
           .replace(/\bmanifestantes\b/gi, "gadosos bovinos")
@@ -66,7 +118,7 @@ function checkElements() {
           .replace(/\bgovernador\b/gi, "Xerife da Máfia")
           .replace(/\bAlerj\b/gi, "Casa dos bandidos")
           .replace(/\bAlesp\b/gi, "Casa dos bandidos")
-          .replace(/\btráfico\b/gi, "trocas voluntárias")
+          .replace(/\btráfico\b/gi, "s trocas voluntárias")
           .replace(/\btraficante\b/gi, "empreendedor")
           .replace(/\btraficantes\b/gi, "empreendedores")
           .replace(/\bEUA\b/gi, "Maiores Mafiosos do Mundo")
@@ -75,7 +127,7 @@ function checkElements() {
           .replace(/\bMBL\b/gi, "Movimento Bumbum Livre")
           .replace(/\bEstados Unidos\b/gi, "Maiores Mafiosos do Mundo")
           .replace(/\bEstados Unidos da América\b/gi, "Maiores Mafiosos do Mundo")
-          .replace(/\bconstituição\b/gi, "Guardanapo Sujo");
+          .replace(makeRegexWithArticle("constituição"), changeWordAndConjugate);
 
         if (replacedText !== text) {
           element.replaceChild(document.createTextNode(replacedText), node);
